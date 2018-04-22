325c5499da8e66e15e9a572af0f468f0bf58cfe8
aleklisi <sebaczechu123@gmail.com>
2018-04-22 17:42:21 +0200

    adding simple tests

--- erlisp/ScanerTests/CodeGeneratorTests.cs
+++ erlisp/ScanerTests/CodeGeneratorTests.cs
@@ -12,6 +12,11 @@ namespace ScanerTests
         [TestCase("(+ 1 2 3)", "fun1() -> sum([1,2,3]).")]
         [TestCase("(+ 1)", "fun1() -> sum([1]).")]
 
+        [TestCase("(write (+ 1 2 3))(write 2)", "fun1() -> write([sum([1,2,3])]).fun2() -> write([2]).")]
+        [TestCase("(write (write (write 2)))", "fun1() -> write([write([write([2])])]).")]
+        [TestCase("(if (write (write 2)) 2 3)", "fun1() -> ifstm([write([write([2])]),2,3]).")]
+        [TestCase("(+ 12 (-) (+ 1 2 (* 3)))", "fun1() -> sum([12,sub([]),sum([1,2,multiply([3])])]).")]
+
         public void TokenRecognizedCorrectly(string input, string expectedCode)
         {
             var skaner = new LispScaner(input);
